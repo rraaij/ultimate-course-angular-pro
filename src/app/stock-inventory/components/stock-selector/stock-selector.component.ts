e0e2e358bd3f0d1a934b619c4ec95c0cb0531d24
Ramon van Raaij <ramon.van.raaij@gmail.com>
2019-11-21 16:30:05 +0100

    binding FormControls to <select>

--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {FormGroup} from '@angular/forms';
+import {Product} from '../../models/product.interface';
 
 @Component({
   selector: 'app-stock-selector',
@@ -8,6 +9,7 @@ import {FormGroup} from '@angular/forms';
 })
 export class StockSelectorComponent {
   @Input() parent: FormGroup;
+  @Input() products: Product[];
 
   constructor() { }
 
--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {Product} from '../models/product.interface';
 
 @Component({
   selector: 'app-stock-inventory',
@@ -7,6 +8,14 @@ import {FormArray, FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./stock-inventory.component.scss']
 })
 export class StockInventoryComponent {
+  products: Product[] = [
+    { id: 1, price: 2800, name: 'MacBook Pro'},
+    { id: 2, price: 50, name: 'USB-C adapter'},
+    { id: 3, price: 400, name: 'iPod'},
+    { id: 4, price: 900, name: 'iPhone'},
+    { id: 5, price: 600, name: 'Apple Watch'},
+  ];
+
   form = new FormGroup({
     store: new FormGroup({
       branch: new FormControl(''),
