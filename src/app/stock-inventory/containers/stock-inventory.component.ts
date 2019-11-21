edca75b2e86c48e246e23cf2d00aceadcdf09064
Ramon van Raaij <ramon.van.raaij@gmail.com>
2019-11-21 17:13:27 +0100

    using FormBuilder instead of FormGroup

--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {FormArray, FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 import {Product} from '../models/product.interface';
 
 @Component({
@@ -16,22 +16,24 @@ export class StockInventoryComponent {
     { id: 5, price: 600, name: 'Apple Watch'},
   ];
 
-  form = new FormGroup({
-    store: new FormGroup({
-      branch: new FormControl(''),
-      code: new FormControl('')
+  form = this.fb.group({
+    store: this.fb.group({
+      branch: '',
+      code: ''
     }),
     selector: this.createStock({}),
-    stock: new FormArray([
+    stock: this.fb.array([
       this.createStock({ product_id: 1, quantity: 10 }),
       this.createStock({ product_id: 3, quantity: 50 }),
     ])
   });
 
+  constructor(private fb: FormBuilder) {}
+
   createStock(stock: any) {
-    return new FormGroup({
-      product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
-      quantity: new FormControl(stock.quantity || 10)
+    return this.fb.group({
+      product_id: parseInt(stock.product_id, 10) || '',
+      quantity: stock.quantity || 10
     });
   }
 
