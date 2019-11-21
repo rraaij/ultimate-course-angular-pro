d7a05da2051e1f0eb497645120a98976767943f0
Ramon van Raaij <ramon.van.raaij@gmail.com>
2019-11-21 15:50:40 +0100

    form controls and form groups

--- src/app/stock-inventory/containers/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-stock-inventory',
@@ -6,5 +7,14 @@ import {Component} from '@angular/core';
   styleUrls: ['./stock-inventory.component.scss']
 })
 export class StockInventoryComponent {
+  form = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl('b182'),
+      code: new FormControl('123')
+    })
+  });
 
+  onSubmit() {
+    console.log('Submit:', this.form.value);
+  }
 }
